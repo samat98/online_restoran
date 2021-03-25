2221b474770187e54302988c69e4c55a17f89e0a
samat98 <samat989a@gmail.com>
2021-03-25 15:53:04 +0600

    профиксил некоторые баги

--- src/main/webapp/app/modules/administration/index.tsx
+++ src/main/webapp/app/modules/administration/index.tsx
@@ -2,11 +2,21 @@ import React from 'react';
 
 import ErrorBoundaryRoute from 'app/shared/error/error-boundary-route';
 import UserManagement from './user-management';
+import LogsPage from "app/modules/administration/logs/logs";
+import HealthPage from "app/modules/administration/health/health";
+import AuditsPage from "app/modules/administration/audits/audits";
+import MetricsPage from "app/modules/administration/metrics/metrics";
+import ConfigurationPage from "app/modules/administration/configuration/configuration";
 import Docs from './docs/docs'
 
 const Routes = ({ match }) => (
   <div>
     <ErrorBoundaryRoute path={`${match.url}/user-management`} component={UserManagement} />
+    <ErrorBoundaryRoute exact path={`${match.url}/logs`} component={LogsPage} />
+    <ErrorBoundaryRoute exact path={`${match.url}/audits`} component={AuditsPage} />
+    <ErrorBoundaryRoute exact path={`${match.url}/configuration`} component={ConfigurationPage} />
+    <ErrorBoundaryRoute exact path={`${match.url}/metrics`} component={MetricsPage} />
+    <ErrorBoundaryRoute exact path={`${match.url}/health`} component={HealthPage} />
     <ErrorBoundaryRoute exact path={`${match.url}/docs`} component={Docs} />
   </div>
 );
